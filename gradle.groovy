9a7d91f218f45dd81bf333ca44bb3baef43c3cfb
Javier Contreras Ferrada <j.contrerasferrada@gmail.com>
2022-01-21 21:27:10 -0300

    add: Agrega archivos groovy y modifica Jenkinsfile

---
+++ gradle.groovy
@@ -0,0 +1,61 @@
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
+
+def call(){
+    stage("build & test"){
+        steps{
+            echo "======== ${env.STAGE_NAME} ========"
+            sh "./gradlew clean build"
+        }
+    }
+    stage("sonar"){
+        steps{
+            script {
+                def scannerHome = tool 'sonar-scanner';
+                withSonarQubeEnv('sonarcube-server') { // If you have configured more than one global server connection, you can specify its name
+                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+                }
+            }
+        }
+    }
+    stage("run"){
+        steps{
+            echo "======== ${env.STAGE_NAME} ========"
+            sh "nohup ./gradlew bootRun &"
+            sh "sleep 10"
+        }
+    }
+    stage("test"){
+        steps{
+            echo "======== ${env.STAGE_NAME} ========"
+            sh "curl -X GET 'http://localhost:8181/rest/mscovid/test?msg=testing'"
+        }
+    }
+    stage("nexus"){
+        steps{
+            echo "======== ${env.STAGE_NAME} ========"
+            nexusPublisher nexusInstanceId: 'test-nexus',
+            nexusRepositoryId: 'ejemplo-gradle',
+            packages: [
+                [
+                    $class: 'MavenPackage',
+                    mavenAssetList: [
+                        [classifier: '', extension: '', filePath: "${env.WORKSPACE}/build/libs/DevOpsUsach2020-0.0.1.jar"]
+                    ],
+                    mavenCoordinate: [
+                        artifactId: 'DevOpsUsach2020',
+                        groupId: 'com.devopsusach2020',
+                        packaging: 'jar',
+                        version: '0.0.1'
+                    ]
+                ]
+            ]
+        }
+    }
+
+}
+
+return this;
---
+++ maven.groovy
@@ -0,0 +1,36 @@
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
+
+def call(){
+    stage("Compile") {
+        steps {
+            sh "./mvnw clean compile -e"    
+        }
+    }
+    stage("Test") {
+        steps {
+            sh "./mvnw clean test -e"    
+        }
+    }
+    stage("Package") {
+        steps {
+            sh "./mvnw clean package -e"    
+        }
+    }
+    stage("Run") {
+        steps {
+            sh "nohup bash mvnw spring-boot:run &"
+            sleep 20
+        }
+    }
+    stage("Test-app") {
+        steps {
+            sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=porfin'"
+        }
+    }
+}
+
+return this;
