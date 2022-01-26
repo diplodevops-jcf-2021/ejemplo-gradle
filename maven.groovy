e8588286f581098642e23640103861a234b6206d
Javier Contreras Ferrada <j.contrerasferrada@gmail.com>
2022-01-26 19:08:37 -0300

    fix: Corrección variable entorno STAGE_NAME

--- maven.groovy
+++ maven.groovy
@@ -6,24 +6,24 @@
 
 def call(){
     stage("Compile") {
-        STAGE = env.STAGE
+        STAGE = env.STAGE_NAME
         sh "./mvnw clean compile -e"    
     }
     stage("Test") {
-        STAGE = env.STAGE
+        STAGE = env.STAGE_NAME
         sh "./mvnw clean test -e"    
     }
     stage("Package") {
-        STAGE = env.STAGE
+        STAGE = env.STAGE_NAME
         sh "./mvnw clean package -e"    
     }
     stage("Run") {
-        STAGE = env.STAGE
+        STAGE = env.STAGE_NAME
         sh "nohup bash mvnw spring-boot:run &"
         sleep 20
     }
     stage("Test-app") {
-        STAGE = env.STAGE
+        STAGE = env.STAGE_NAME
         sh "curl -X GET 'http://localhost:8181/rest/mscovid/test?msg=porfin'"
     }
 }
